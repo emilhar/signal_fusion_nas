d9d6a9b714f24f2214514815caaec5c13faeec79
Ómar Bessi Ómarsson <omarbessi04@gmail.com>
2025-06-26 12:58:19 +0000

    kill KILL

--- SLEAP/Globals.py
+++ SLEAP/Globals.py
@@ -189,29 +189,6 @@ class UniquenessFunctions:
         transition = 40
 
         return 1 / (1 + math.exp(-steepness * (min_distance - transition)))
-                
-
-    
-    @staticmethod
-    def KILL(individual, comparisons):
-
-        max_possible_dist = ModelSettings.KERNELS_PER_BRANCH * (
-                ModelSettings.MAX_KERNEL_SIZE - ModelSettings.MIN_KERNEL_SIZE
-            )
-
-        def distance(a, b):
-            dist = max_possible_dist - sum(abs(x - y) for x, y in zip(a, b))
-            return dist / max_possible_dist
-
-        uniqueness_scores = [
-            distance(individual[0], other[0])
-            for other in comparisons
-        ]
-        sum_denominator = sum(uniqueness_scores)
-
-        uniqueness = 1 / (1/sum_denominator) if sum_denominator != 0 else 0.0
-        return uniqueness
-
 
     uniqueness_function = gargoyle
 
